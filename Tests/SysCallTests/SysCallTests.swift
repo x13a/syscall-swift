aec43d95dc8bfa66270b0040ad0dfb7b4cc6a1c4
lucky <>
2021-07-24 00:27:43 +0300

    add test case

--- Tests/SysCallTests/SysCallTests.swift
+++ Tests/SysCallTests/SysCallTests.swift
@@ -2,7 +2,20 @@
     @testable import SysCall
 
     final class SysCallTests: XCTestCase {
-        func testExample() {
-
+        func testKinfo() throws {
+            let kinfo = try SysCall.kinfo(getpid()).get()
+            print(kinfo)
+        }
+        
+        func testPpid() throws {
+            let ppid = try SysCall.ppid(getpid()).get()
+            assert(ppid == getppid())
+        }
+        
+        func testArgs() throws {
+            let args = try SysCall.args(getpid()).get()
+            print(args.path)
+            print(args.args)
+            print(args.env)
         }
     }
